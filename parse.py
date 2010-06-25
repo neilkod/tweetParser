6e070c4edc4ae06133eacf081d47969ad24c39bb
neil kodner <nkodner@gmail.com>
2010-06-25 06:54:16 -0400

    added parse counts for file-based processing

--- parse.py
+++ parse.py
@@ -44,22 +44,30 @@ def parseTweet(jsondata,logFileHandle,badFileHandle):
       """
       text = '%d\t%s\t%s\t%s\t%s' % (dict['id'],dict['created_at'],client,dict['user']['screen_name'],tweetText)
 
-      writeToLog(logFileHandle,text)    
+      writeToLog(logFileHandle,text)
+      return 1
 
   except:
     None
     # if we can't convert the json to a dict, lets log it and skip the row.
     # ignore the bad for now
-    #writeToLog(badFileHandle,line+'\n')      
+    #writeToLog(badFileHandle,line+'\n')  
+    return 0
 
 def parseFile(inputFile):
 
+  parsed = 0
   cnt = 0
-  logFileHandle = codecs.open(inputFile + '.out' ,'a','utf-8')
-  badFileHandle = codecs.open(inputFile + '.bad','a','utf-8')
+  # for the time being, we'll just assume the outputfile will be inputfile.out
+  # and the badfile will be outputfile.bad.  we'll make this more dynamic later.
+  logFileHandle = codecs.open(inputFile + '.out' ,'w','utf-8')
+  badFileHandle = codecs.open(inputFile + '.bad','w','utf-8')
   
   for line in file(inputFile):
-    parseTweet(line,logFileHandle,badFileHandle)
+    parsed = parsed + parseTweet(line,logFileHandle,badFileHandle)
+    cnt = cnt + 1
+
+  print "%s: parsed %d tweets out of %d in file" % (inputFile,parsed,cnt)    
 
     
 if __name__ == '__main__':
@@ -71,9 +79,7 @@ if __name__ == '__main__':
 
   for line in sys.stdin:
     parseTweet(line,logFileHandle,badFileHandle)
-    cnt = cnt + 1
 
-  print "%s parsed %d tweets" % (logFileName,cnt)
   logFileHandle.close()
   badFileHandle.close()
     
